1a7d121fc332937772037555c8e4e18ea2628d7c
xu rui <xurui1@pjlab.org.cn>
2023-02-14 22:26:30 +0800

    fix compile

---
+++ index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import MyView from './Components/MyView';
+
+createRoot(document.getElementById('react-root')).render(<MyView />);
